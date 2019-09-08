67b87aca6631e16654916fc10e18f39c138af4f5
lishiqi <786008752@qq.com>
2019-09-08 21:51:11 +0800

    整个学校信息项目

---
+++ src/main/java/pojo/Teacher.java
@@ -0,0 +1,37 @@
+package com.atong.entity;
+
+import java.io.Serializable;
+
+public class Teacher implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Integer classid;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getClassid() {
+        return classid;
+    }
+
+    public void setClassid(Integer classid) {
+        this.classid = classid;
+    }
+}
